2bae4933159f80d26814e191232ca6bda37929f3
Christian Witte <christian.witte@gmail.com>
2014-07-08 21:30:26 -0500

    Replaced java-style imperative code with functional Levenshtein distance calculator from rosettacode.org -- this will keep scalastyle from complaining.

--- src/main/scala/org/ensime/util/StringSimilarity.scala
+++ src/main/scala/org/ensime/util/StringSimilarity.scala
@@ -1,79 +1,22 @@
 package org.ensime.util
 
+import scala.math.min
+
 trait StringSimilarity {
 
   def getLevenshteinDistance(s: String, t: String): Int = {
 
-    if (s == null || t == null) {
-      throw new IllegalArgumentException("Strings must not be null")
+    // Code from: http://rosettacode.org/wiki/Levenshtein_distance#Scala
+    def minimum(i1: Int, i2: Int, i3: Int): Int = min(min(i1, i2), i3)
+    def distance(s1: String, s2: String): Int = {
+      val dist = Array.tabulate(s2.length + 1, s1.length + 1) { (j, i) => if (j == 0) i else if (i == 0) j else 0 }
+
+      for (j <- 1 to s2.length; i <- 1 to s1.length)
+        dist(j)(i) = if (s2(j - 1) == s1(i - 1)) { dist(j - 1)(i - 1) }
+        else { minimum(dist(j - 1)(i) + 1, dist(j)(i - 1) + 1, dist(j - 1)(i - 1) + 1) }
+
+      dist(s2.length)(s1.length)
     }
-
-    /*
-    The difference between this impl. and the previous is that, rather 
-    than creating and retaining a matrix of size s.length()+1 by t.length()+1, 
-    we maintain two single-dimensional arrays of length s.length()+1.  The first, d,
-    is the 'current working' distance array that maintains the newest distance cost
-    counts as we iterate through the characters of String s.  Each time we increment
-    the index of String t we are comparing, d is copied to p, the second int[].  Doing so
-    allows us to retain the previous cost counts as required by the algorithm (taking 
-    the minimum of the cost count to the left, up one, and diagonally up and to the left
-    of the current cost count being calculated).  (Note that the arrays aren't really 
-    copied anymore, just switched...this is clearly much better than cloning an array 
-    or doing a System.arraycopy() each time  through the outer loop.)
-
-    Effectively, the difference between the two implementations is this one does not 
-    cause an out of memory condition when calculating the LD over two very large strings.  		
-    */
-
-    val n = s.length // length of s
-    val m = t.length // length of t
-
-    if (n == 0) {
-      return m
-    } else if (m == 0) {
-      return n
-    }
-
-    var p = new Array[Int](n + 1) //'previous' cost array, horizontally
-    var d = new Array[Int](n + 1) // cost array, horizontally
-    var _d: Array[Int] = null //placeholder to assist in swapping p and d
-
-    // indexes into strings s and t
-    var i: Int = 0 // iterates through s
-    var j: Int = 0 // iterates through t
-
-    var t_j: Char = 0 // jth character of t
-
-    var cost: Int = 0 // cost
-
-    i = 0
-    while (i <= n) {
-      p(i) = i
-      i += 1
-    }
-
-    j = 1
-    while (j <= m) {
-      t_j = t.charAt(j - 1)
-      d(0) = j
-
-      i = 1
-      while (i <= n) {
-        cost = if (s.charAt(i - 1) == t_j) 0 else 1
-        // minimum of cell to the left+1, to the top+1, diagonally left and up +cost				
-        d(i) = scala.math.min(scala.math.min(d(i - 1) + 1, p(i) + 1), p(i - 1) + cost)
-        i += 1
-      }
-
-      // copy current distance counts to 'previous row' distance counts
-      _d = p
-      p = d
-      d = _d
-      j += 1
-    }
-
-    // our last action in the above loop was to switch d and p, so p now 
-    // actually has the most recent cost counts
-    p(n)
+    distance(s, t)
   }
 }
